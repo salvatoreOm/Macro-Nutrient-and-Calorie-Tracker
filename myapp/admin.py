7bdc1220c7ff05a55693e900be850357d50d4b65
Prakhar-1401 <prakharmittalgwl@gmail.com>
2025-01-15 10:40:29 +0530

    Prak first commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Food
+from .models import Supplements
+from .models import CaloriesBurnt
 # Register your models here.
 admin.site.register(Food)
+admin.site.register(Supplements)
+admin.site.register(CaloriesBurnt)
--- myapp/models.py
+++ myapp/models.py
@@ -11,3 +11,21 @@ class Food(models.Model):
     protein = models.FloatField()
     fats = models.FloatField()
     calories = models.IntegerField()
+
+class Supplements(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    protein = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+class CaloriesBurnt(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100)
+    caloriesburnt= models.IntegerField()
